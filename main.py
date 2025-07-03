c48ebc1a80c6bf2edfeacfba26fcf2fe6e1616e8
Nollingd <151438246+Nollingd@users.noreply.github.com>
2025-07-03 22:48:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-!pip install pyngrok
-# Tambahkan ngrok authtoken (GANTI dengan milikmu!)
-!ngrok config add-authtoken 2q21bZn5nQFgMwrK59fhSXFqOIB_48NSHVSpNLZtQ4CfjUhz3
-
-from pyngrok import ngrok
-
-# Buat tunnel dengan syntax yang benar:
-public_url = ngrok.connect("http://localhost:8501")
-print("🌐 Ngrok tunnel:", public_url)
-
-# Jalankan Streamlit (background)
-!streamlit run form.py --server.port 8501
+streamlit run \form\form.py
